9284b8dcc58d9af625ff1cfaec07da16347e0266
oushihao <oushihao97@163.com>
2022-03-19 18:11:02 +0800

    chore: lazy routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '~/pages/home.vue'
-import about from '~/pages/about.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     name: 'Home',
     path: '/',
-    component: Home,
+    component: () => import('~/pages/home.vue'),
   },
   {
     name: 'About',
     path: '/about',
-    component: about,
+    component: () => import('~/pages/about.vue'),
   },
 ]
 
