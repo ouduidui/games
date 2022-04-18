59a352b7fa0ce6d74fcd030d3dfd5a4358595215
Dewey Ou <oushihao97@gmail.com>
2022-04-18 16:17:41 +0800

    feat: custom statusbar

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// #ifdef MP-WEIXIN
+const filterBoxTop = ref(`${uni.getSystemInfoSync().statusBarHeight + 45}px`)
+// #endif
+</script>
+
+<template>
+  <!-- #ifdef MP-WEIXIN -->
+  <view class="status-bar" />
+  <!-- #endif -->
+</template>
+
+<style scope>
+.status-bar {
+  height: v-bind(filterBoxTop);
+  width: 100%
+}
+</style>
