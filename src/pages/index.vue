5c4e7e05df78be083dcfe6bcfe8d7790cae96500
Dewey Ou <oushihao97@gmail.com>
2022-07-16 21:54:07 +0800

    feat: initial

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,4 @@
 <script setup lang="ts">
-import { todosStore } from '~/stores/index'
-
-const store = todosStore()
-const { todos, doneTodos } = storeToRefs(store)
-const doneHandle = (id: number, done: boolean) => store.todoDone(id, done)
-
-const inputContent = ref('')
-
-const addTodoHandle = () => {
-  if (inputContent.value) {
-    store.addTodo(inputContent.value)
-    inputContent.value = ''
-  }
-}
 </script>
 
 <template>
@@ -24,38 +10,7 @@ const addTodoHandle = () => {
     items-center
   >
     <div text-3xl pb-10>
-      Todo List
-    </div>
-
-    <input
-      v-model="inputContent"
-      placeholder="add a todo"
-      type="text"
-      block
-      p="x-4 y-2"
-      mb-8
-      w-250px
-      text-center
-      bg-transparent
-      outline="none active:none"
-      border="~ rounded gray-700 dark:gray-200"
-      @keyup.enter="addTodoHandle"
-    >
-
-    <div>
-      <todo-item
-        v-for="todo in todos"
-        :key="todo.id"
-        :todo="todo"
-        @done-handle="doneHandle"
-      />
-      <todo-item
-        v-for="todo in doneTodos"
-        :key="todo.id"
-        done
-        :todo="todo"
-        @done-handle="doneHandle"
-      />
+      HelloWorld
     </div>
   </main>
 </template>
